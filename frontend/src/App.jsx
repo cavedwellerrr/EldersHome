c1df677eda66da4044128450c93e35bcd956bd55
Lakkhana <lakkhanam05@gmail.com>
2025-09-01 19:47:17 +0530

    basic pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/guardian/Login";
+import Register from "./pages/guardian/Register";
+import Profile from "./pages/guardian/Profile";
+
 
 const App = () => {
   return (
-
     <>
-    <button className="btn btn-primary">CLick me</button>
-    <div className="text-red-500">App</div>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      {/* <Footer /> */}
     </>
-    
-  )
-}
 
-export default App
+  );
+};
+
+export default App;
