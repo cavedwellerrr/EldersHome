40134a58ea4358f69e7e58d6577aa9dea1f92b05
Oshaniderun <florancederun@gmail.com>
2025-09-05 12:06:31 +0530

    meal and room management

---
+++ backend/models/elder_meal_model.js
@@ -0,0 +1,28 @@
+// models/elder_meal_preference_model.js
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const selectionsSchema = new Schema(
+    {
+        breakfast: [{ type: Schema.Types.ObjectId, ref: "Meal" }],
+        lunch: [{ type: Schema.Types.ObjectId, ref: "Meal" }],
+        dinner: [{ type: Schema.Types.ObjectId, ref: "Meal" }],
+        snacks: [{ type: Schema.Types.ObjectId, ref: "Meal" }],
+    },
+    { _id: false }
+);
+
+const elderMealPreferenceSchema = new Schema(
+    {
+        elder: { type: Schema.Types.ObjectId, ref: "Elder", required: true, unique: true },
+        allergies: [String],
+        selections: { type: selectionsSchema, default: () => ({}) },
+        notes: String,
+        lastUpdatedBy: { type: Schema.Types.ObjectId, ref: "Staff" },
+    },
+    { timestamps: true }
+);
+
+elderMealPreferenceSchema.index({ elder: 1 }, { unique: true });
+
+export default mongoose.model("ElderMealPreference", elderMealPreferenceSchema);
